20ea3afbc27aba588ec8bb9aa44a30a159a52232
Yi-Hung Wei <yihung.wei@gmail.com>
2019-11-07 12:28:26 -0800

    lib: Build library and export parse functions

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+#![allow(dead_code,
+         unused_variables,
+         unused_assignments,
+         non_camel_case_types,
+         unused_mut)]
+
+#![feature(stmt_expr_attributes)]
+
+pub mod dp_packet;
+pub mod flow;
+pub mod miniflow;
+pub mod packet;
+pub mod parser;
+pub mod tun_metadata;
+pub mod types;
+
+use crate::dp_packet::*;
+use crate::miniflow::*;
+use crate::parser::*;
+use std::slice;
+
+#[no_mangle]
+pub extern "C" fn rust_miniflow_extract(packet: *mut Dp_packet,
+                                        dst: *mut Miniflow) {
+    println!("Hello from rust miniflow extract!");
+
+    let mut mf: Miniflow = Miniflow::new();
+    let mut mfx = &mut mf_ctx::from_mf(mf.map, &mut mf.values);
+
+    unsafe {
+        let md = &(*packet).data_.md;
+        let data = (*packet).dp_packet_data();
+        let packet_type_be =  (*packet).packet_type;
+
+        parse_metadata(md, packet_type_be, mfx);
+        let result = parse_l2(data, mfx, packet_type_be);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn test_data(data: *const libc::c_void) {
+    println!("Hello from rust test_data!");
+
+    unsafe {
+        let slice = slice::from_raw_parts(data.offset(3) as *const u8, 5);
+
+        println!("data[0] = {}, data[3] = {}", slice[0], slice[3]);
+    }
+}
